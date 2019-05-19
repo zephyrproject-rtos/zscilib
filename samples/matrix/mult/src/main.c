2fc1daa37fc6785650d1c4b3d5a307dc43a71495
microbuilder <kevin@ktownsend.com>
2019-05-19 10:33:09 +0200

    Stylistic cleanup

--- samples/matrix/mult/src/main.c
+++ samples/matrix/mult/src/main.c
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 #include <zephyr.h>
 #include <misc/printk.h>
 #include <zsl/zsl.h>
@@ -13,37 +12,39 @@
 void test_mtx_mult_demo(void)
 {
         int rc;
-        zsl_real_t ma_data[12] = {
-            1.0, 2.0, 3.0,
-            4.0, 5.0, 6.0,
-            7.0, 8.0, 9.0,
-            1.0, 1.0, 1.0
-        };
-
-        zsl_real_t mb_data[12] = {
-            1.0, 2.0, 3.0, 4.0,
-            5.0, 6.0, 7.0, 8.0,
-            1.0, 1.0, 1.0, 1.0
-        };
 
         ZSL_MATRIX_DEF(ma, 4, 3);   /* 4x3 matrix */
         ZSL_MATRIX_DEF(mb, 3, 4);   /* 3x4 matrix */
         ZSL_MATRIX_DEF(mc, 4, 4);   /* Resulting 4x4 matrix. */
+
+        zsl_real_t ma_data[12] = {
+                1.0, 2.0, 3.0,
+                4.0, 5.0, 6.0,
+                7.0, 8.0, 9.0,
+                1.0, 1.0, 1.0
+        };
+
+        zsl_real_t mb_data[12] = {
+                1.0, 2.0, 3.0, 4.0,
+                5.0, 6.0, 7.0, 8.0,
+                1.0, 1.0, 1.0, 1.0
+        };
 
         /* Assign data to input matrices. */
         ma.data = ma_data;
         mb.data = mb_data;
 
         /* Initialise output matrix. */
-        rc = zsl_mtx_init(&mc, NULL);
+        zsl_mtx_init(&mc, NULL);
 
+        /* Multiple ma x mb. */
         rc = zsl_mtx_mult(&ma, &mb, &mc);
         if (rc) {
-            printk("Error performing matrix multuplication: %d\n", rc);
-            return;
+                printk("Error performing matrix multuplication: %d\n", rc);
+                return;
         }
 
-        printk("mtx multuply output:\n");
+        printk("mtx multuply output:\n\n");
         zsl_mtx_print(&mc);
 }
 
