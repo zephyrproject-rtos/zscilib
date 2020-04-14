7ece2b1d5ecd858af6716ac48e81452cf979f306
Kevin Townsend <kevin@ktownsend.com>
2020-04-14 16:58:19 +0200

    samples: Update for Zephyr 2.2
    This commit makes some minor updates to account for changes in
    Zephyr 2.2+.
    
    Signed-off-by: Kevin Townsend <kevin@ktownsend.com>

--- samples/benchmarking/src/main.c
+++ samples/benchmarking/src/main.c
@@ -6,17 +6,18 @@
 
 
 #include <zephyr.h>
-#include <misc/printk.h>
+#include <sys/printk.h>
 #include <zsl/zsl.h>
 #include <zsl/vectors.h>
 
-#if !CONFIG_BOARD_QEMU_CORTEX_M3 && (CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
+#if !CONFIG_BOARD_QEMU_CORTEX_M3 && \
+	(CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
 /* Use DWT cycle counter for precision timing if this is a HW M3/M4 core. */
 u32_t dwt_count;
-#define DWT_RESET_CYCLECOUNTER    do {                     \
-        CoreDebug->DEMCR = CoreDebug->DEMCR | 0x01000000;  \
-        DWT->CYCCNT = 0;                                   \
-        DWT->CTRL = DWT->CTRL | 1 ; } while(0)
+#define DWT_RESET_CYCLECOUNTER    do {				  \
+		CoreDebug->DEMCR = CoreDebug->DEMCR | 0x01000000; \
+		DWT->CYCCNT = 0;				  \
+		DWT->CTRL = DWT->CTRL | 1; } while(0)
 #else
 /* Use the high-precision kernel clock. */
 u32_t start_time;
@@ -30,84 +31,87 @@ u32_t nanoseconds_spent;
 
 void print_settings(void)
 {
-        printk("BOARD:                       %s\n", CONFIG_BOARD);
-        printk("ZSL VERSION:                 %s (%s)\n", ZSL_VERSION,
-               ZSL_VERSION_DATE);
+	printk("BOARD:                       %s\n", CONFIG_BOARD);
+	printk("ZSL VERSION:                 %s (%s)\n", ZSL_VERSION,
+	       ZSL_VERSION_DATE);
 
-        printk("CONFIG_ZSL_PLATFORM_OPT:     %i\n", CONFIG_ZSL_PLATFORM_OPT);
+	printk("CONFIG_ZSL_PLATFORM_OPT:     %i\n", CONFIG_ZSL_PLATFORM_OPT);
 #if CONFIG_ZSL_SINGLE_PRECISION
-        printk("CONFIG_ZSL_SINGLE_PRECISION: True\n");
+	printk("CONFIG_ZSL_SINGLE_PRECISION: True\n");
 #else
-        printk("CONFIG_ZSL_SINGLE_PRECISION: False\n");
+	printk("CONFIG_ZSL_SINGLE_PRECISION: False\n");
 #endif
 #if CONFIG_ZSL_VECTOR_INLINE
-        printk("CONFIG_ZSL_VECTOR_INLINE:    True\n");
+	printk("CONFIG_ZSL_VECTOR_INLINE:    True\n");
 #else
-        printk("CONFIG_ZSL_VECTOR_INLINE:    False\n");
+	printk("CONFIG_ZSL_VECTOR_INLINE:    False\n");
 #endif
 #if CONFIG_ZSL_MATRIX_INLINE
-        printk("CONFIG_ZSL_MATRIX_INLINE:    True\n");
+	printk("CONFIG_ZSL_MATRIX_INLINE:    True\n");
 #else
-        printk("CONFIG_ZSL_MATRIX_INLINE:    False\n");
+	printk("CONFIG_ZSL_MATRIX_INLINE:    False\n");
 #endif
 #if CONFIG_ZSL_BOUNDS_CHECKS
-        printk("CONFIG_ZSL_BOUNDS_CHECKS:    True\n");
+	printk("CONFIG_ZSL_BOUNDS_CHECKS:    True\n");
 #else
-        printk("CONFIG_ZSL_BOUNDS_CHECKS:    False\n");
+	printk("CONFIG_ZSL_BOUNDS_CHECKS:    False\n");
 #endif
-        printk("\n");
+	printk("\n");
 }
 
 void test_vec_add(void)
 {
-        ZSL_VECTOR_DEF(va, 3);
-        ZSL_VECTOR_DEF(vb, 3);
-        ZSL_VECTOR_DEF(vc, 3);
+	ZSL_VECTOR_DEF(va, 3);
+	ZSL_VECTOR_DEF(vb, 3);
+	ZSL_VECTOR_DEF(vc, 3);
 
-        zsl_real_t a[3] = { 0.0, 1.0, 2.0 };
-        zsl_real_t b[3] = {-1.0, 0.5, 0.1 };
+	zsl_real_t a[3] = { 0.0, 1.0, 2.0 };
+	zsl_real_t b[3] = { -1.0, 0.5, 0.1 };
 
-        /* Assign arrays to vectors. */
-        zsl_vec_from_arr(&va, a);
-        zsl_vec_from_arr(&vb, b);
+	/* Assign arrays to vectors. */
+	zsl_vec_from_arr(&va, a);
+	zsl_vec_from_arr(&vb, b);
 
-        /* Init vc with zero values. */
-        zsl_vec_init(&vc);
+	/* Init vc with zero values. */
+	zsl_vec_init(&vc);
 
-#if !CONFIG_BOARD_QEMU_CORTEX_M3 && (CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
-        DWT_RESET_CYCLECOUNTER;
+#if !CONFIG_BOARD_QEMU_CORTEX_M3 && \
+	(CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
+	DWT_RESET_CYCLECOUNTER;
 #else
-        /* Perform the add operation, tracking start and end times. */
-        start_time = k_cycle_get_32();
+	/* Perform the add operation, tracking start and end times. */
+	start_time = k_cycle_get_32();
 #endif
 
-        for (u32_t i = 0; i < BENCH_LOOPS; i++) {
-                zsl_vec_add(&va, &vb, &vc);
-        }
+	for (u32_t i = 0; i < BENCH_LOOPS; i++) {
+		zsl_vec_add(&va, &vb, &vc);
+	}
 
-#if !CONFIG_BOARD_QEMU_CORTEX_M3 && (CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
-        dwt_count = DWT->CYCCNT;
+#if !CONFIG_BOARD_QEMU_CORTEX_M3 && \
+	(CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
+	dwt_count = DWT->CYCCNT;
 #else
-        stop_time = k_cycle_get_32();
-        /* compute how long the work took (assumes no counter rollover) */
-        cycles_spent = stop_time - start_time;
-        nanoseconds_spent = SYS_CLOCK_HW_CYCLES_TO_NS(cycles_spent);
+	stop_time = k_cycle_get_32();
+	/* compute how long the work took (assumes no counter rollover) */
+	cycles_spent = stop_time - start_time;
+	nanoseconds_spent = (u32_t)k_cyc_to_ns_floor64(cycles_spent);
 #endif
 
-#if !CONFIG_BOARD_QEMU_CORTEX_M3 && (CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
-        printk("zsl_vec_add : %u cycles\n", dwt_count / BENCH_LOOPS);
+#if !CONFIG_BOARD_QEMU_CORTEX_M3 && \
+	(CONFIG_CPU_CORTEX_M3 || CONFIG_CPU_CORTEX_M4)
+	printk("zsl_vec_add : %u cycles\n", dwt_count / BENCH_LOOPS);
 #else
-        printk("zsl_vec_add : %u ns\n", nanoseconds_spent / BENCH_LOOPS);
+	printk("zsl_vec_add : %u ns\n", nanoseconds_spent / BENCH_LOOPS);
 #endif
 }
 
 void main(void)
 {
-        printk("zscilib benchmark\n\n");
-        print_settings();
+	printk("zscilib benchmark\n\n");
+	print_settings();
 
-        while (1) {
-                test_vec_add();
-                k_sleep(5000);
-        }
+	while (1) {
+		test_vec_add();
+		k_sleep(5000);
+	}
 }
