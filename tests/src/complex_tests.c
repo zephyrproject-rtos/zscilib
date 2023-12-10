e1ec81f8c88613fe80963ad5555b2a370af0352f
Ryan McClelland <ryanmcclelland@meta.com>
2023-12-10 11:31:46 -0800

    fix complex_tests for single and double percision
    Float and Double were swapped for their tests. `I` is a single
    percision constant so cast to double.
    
    Signed-off-by: Ryan McClelland <ryanmcclelland@meta.com>

--- tests/src/complex_tests.c
+++ tests/src/complex_tests.c
@@ -22,13 +22,13 @@ ZTEST(zsl_tests, test_complex_add)
 {
 	/* NOTE: zsl_real_t typedef can't be used with C99 complex numbers. */
 #if CONFIG_ZSL_SINGLE_PRECISION
-	double complex a = 1.0 + 0.4 * I;
-	double complex b = 1.0 + 0.5 * I;
-	double complex c = 0.0;
-#else
 	float complex a = 1.0 + 0.4 * I;
 	float complex b = 1.0 + 0.5 * I;
 	float complex c = 0.0;
+#else
+	double complex a = 1.0 + 0.4 * (double)I;
+	double complex b = 1.0 + 0.5 * (double)I;
+	double complex c = 0.0;
 #endif
 
 	c = a + b;
